ca4620a9378cd84e6acb27b607dfc4c0e0eae537
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-10-27 15:54:56 -0300

    style: extend ESLint configuration rules

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,17 +3,17 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-	title: 'Rodrigo Fontes',
+  title: 'Rodrigo Fontes | Desenvolvedor Front-End',
 }
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode
+  children: React.ReactNode
 }>) {
-	return (
-		<html lang="pt-BR">
-			<body>{children}</body>
-		</html>
-	)
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  )
 }
