2e70f5b71f64f099ec35aad93802cbafff74f458
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-10-30 16:40:15 -0300

    refactor(component): change pseudo-elements for <span> tags in main menu
    Improved code readability and maintainability using this manner.

--- src/components/MainMenu/index.tsx
+++ src/components/MainMenu/index.tsx
@@ -16,7 +16,7 @@ type MenuLinkProps = {
 
 function MenuRoot({ children }: MenuRootProps) {
   return (
-    <div className="h-full relative">
+    <div className="h-full">
       <nav className="w-full absolute bottom-0">
         <ul>{children}</ul>
       </nav>
@@ -28,10 +28,11 @@ function MenuItem({ children, isActive }: MenuItemProps) {
   return (
     <li
       data-active={isActive}
-      className="border-t border-t-white/10 last:border-b last:border-b-transparent hover:bg-[#0F0F0F] data-[active=true]:bg-[#0F0F0F] transition-all duration-500
-      *:data-[active=true]:before:content-[''] *:data-[active=true]:before:w-2 *:data-[active=true]:before:h-2 *:data-[active=true]:before:bg-transparent *:data-[active=true]:before:absolute *:data-[active=true]:before:-top-px *:data-[active=true]:before:-right-px *:data-[active=true]:before:border-t *:data-[active=true]:before:border-r *:data-[active=true]:before:border-white *:data-[active=true]:before:transition-colors *:data-[active=true]:before:duration-500 *:data-[active=true]:after:content-[''] *:data-[active=true]:after:w-2 *:data-[active=true]:after:h-2 *:data-[active=true]:after:bg-transparent *:data-[active=true]:after:absolute *:data-[active=true]:after:-bottom-px *:data-[active=true]:after:-right-px *:data-[active=true]:after:border-b *:data-[active=true]:after:border-r *:data-[active=true]:after:border-white *:data-[active=true]:after:transition-colors *:data-[active=true]:after:duration-500"
+      className="relative group border-t border-t-white/10 last:border-b last:border-b-transparent hover:bg-[#0F0F0F] data-[active=true]:bg-[#0F0F0F] transition-all duration-500"
     >
+      <span className="w-2 h-2 absolute -top-px -right-px border-t border-r border-transparent group-hover:border-white group-data-[active=true]:border-white transition-colors duration-500" />
       {children}
+      <span className="w-2 h-2 absolute -bottom-px -right-px border-b border-r border-transparent group-hover:border-white group-data-[active=true]:border-white transition-colors duration-500" />
     </li>
   )
 }
@@ -39,7 +40,7 @@ function MenuItem({ children, isActive }: MenuItemProps) {
 function MenuLink({ text, ...props }: MenuLinkProps) {
   return (
     <Link
-      className="block relative py-6 text-sm text-center font-semibold uppercase before:content-[''] before:w-2 before:h-2 before:bg-transparent before:absolute before:-top-px before:-right-px before:border-t before:border-r before:border-transparent before:transition-colors before:duration-500 hover:before:border-white after:content-[''] after:w-2 after:h-2 after:bg-transparent after:absolute after:-bottom-px after:-right-px after:border-b after:border-r after:border-transparent after:transition-colors after:duration-500 hover:after:border-white"
+      className="block py-6 text-sm text-center font-semibold uppercase"
       {...props}
     >
       {text}
