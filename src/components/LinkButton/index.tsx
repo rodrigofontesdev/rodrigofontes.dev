06b4c6082bd90fc494d4977ccf600833728409b5
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-11-02 21:23:07 -0300

    style(component): fix coding standards

--- src/components/LinkButton/index.tsx
+++ src/components/LinkButton/index.tsx
@@ -10,7 +10,8 @@ export function LinkButton({ appearance = 'transparent', children, ...props }: L
   return (
     <a
       className={twMerge(
-        'group relative text-sm hover:text-black font-semibold uppercase hover:bg-white border border-white/10 py-5 px-10 transition-colors duration-500',
+        'group relative text-sm hover:text-black font-semibold uppercase hover:bg-white',
+        'border border-white/10 py-5 px-10 transition-colors duration-500',
         appearance === 'solid'
           ? 'bg-white text-black'
           : appearance === 'translucide'
@@ -20,11 +21,27 @@ export function LinkButton({ appearance = 'transparent', children, ...props }: L
       role="button"
       {...props}
     >
-      <span className="w-2 h-2 absolute -top-px -left-px border-t-2 border-l-2 border-white group-hover:border-black group-hover:transform group-hover:translate-y-1 group-hover:translate-x-1 transition-all duration-500" />
-      <span className="w-2 h-2 absolute -bottom-px -left-px border-b-2 border-l-2 border-white group-hover:border-black group-hover:transform group-hover:-translate-y-1 group-hover:translate-x-1 transition-all duration-500" />
+      <span
+        className="w-2 h-2 absolute -top-px -left-px border-t-2 border-l-2 border-white
+      group-hover:border-black group-hover:transform group-hover:translate-y-1
+      group-hover:translate-x-1 transition-all duration-500"
+      />
+      <span
+        className="w-2 h-2 absolute -bottom-px -left-px border-b-2 border-l-2 border-white
+       group-hover:border-black group-hover:transform group-hover:-translate-y-1
+       group-hover:translate-x-1 transition-all duration-500"
+      />
       {children}
-      <span className="w-2 h-2 absolute -top-px -right-px border-t-2 border-r-2 border-white group-hover:border-black group-hover:transform group-hover:translate-y-1 group-hover:-translate-x-1 transition-all duration-500" />
-      <span className="w-2 h-2 absolute -bottom-px -right-px border-b-2 border-r-2 border-white group-hover:border-black group-hover:transform group-hover:-translate-y-1 group-hover:-translate-x-1 transition-all duration-500" />
+      <span
+        className="w-2 h-2 absolute -top-px -right-px border-t-2 border-r-2 border-white
+      group-hover:border-black group-hover:transform group-hover:translate-y-1
+      group-hover:-translate-x-1 transition-all duration-500"
+      />
+      <span
+        className="w-2 h-2 absolute -bottom-px -right-px border-b-2 border-r-2 border-white
+       group-hover:border-black group-hover:transform group-hover:-translate-y-1
+       group-hover:-translate-x-1 transition-all duration-500"
+      />
     </a>
   )
 }
