cd6cd9f3feeb712fe48a15fe845cd0fda2245991
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-11-02 21:26:46 -0300

    feat(component): check active menu item in Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,15 @@
+'use client'
+
 import Image from 'next/image'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import { MainMenu } from '../MainMenu'
 
 import logoLightImage from '@/assets/logo-light.svg'
 
 export function Header() {
+  const route = usePathname()
+
   return (
     <header className="fixed top-0 bottom-0 flex flex-col border-r border-r-white/10">
       <div className="h-20 flex border-b border-b-white/10">
@@ -22,24 +27,20 @@ export function Header() {
       </div>
 
       <MainMenu.Root>
-        <MainMenu.Item isActive>
-          <Link href="#">Home</Link>
+        <MainMenu.Item isActive={route === '/'}>
+          <Link href="/">Home</Link>
         </MainMenu.Item>
 
-        <MainMenu.Item>
-          <Link href="#">Sobre</Link>
+        <MainMenu.Item isActive={route === '/experiencia'}>
+          <Link href="/experiencia">Experiência</Link>
         </MainMenu.Item>
 
-        <MainMenu.Item>
-          <Link href="#">Experiência</Link>
+        <MainMenu.Item isActive={route === '/projetos'}>
+          <Link href="/projetos">Projetos</Link>
         </MainMenu.Item>
 
-        <MainMenu.Item>
-          <Link href="#">Projetos</Link>
-        </MainMenu.Item>
-
-        <MainMenu.Item>
-          <Link href="#">Contato</Link>
+        <MainMenu.Item isActive={route === '/contato'}>
+          <Link href="/contato">Contato</Link>
         </MainMenu.Item>
       </MainMenu.Root>
     </header>
